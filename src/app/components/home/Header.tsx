0826150904103cf464e4df09c69cdad32fd6616d
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2024-11-01 01:28:17 +0700

    base structure~5 20241101_0135

--- src/app/components/home/Header.tsx
+++ src/app/components/home/Header.tsx
@@ -8,7 +8,12 @@ const Header = () => (
       <div className="text-xl font-bold">ПОРТАЛ СИТИ-КВАРТАЛ ОКТЯБРЬСКИЙ</div>
     </div>
     <nav className="space-x-6">
-      <Link href="/auth/login" className="text-gray-600 hover:text-gray-900">Войти</Link>
+      <button
+        type="submit"
+        className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+      >
+        Войти в ЛК
+      </button>
     </nav>
   </header>
 );
